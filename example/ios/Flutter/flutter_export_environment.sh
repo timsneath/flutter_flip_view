ef6dbc20af3b3602a27f9afbcc7068f37f1aad11
Tim Sneath <timsneath@timsneath-macbookair.roam.corp.google.com>
2019-08-14 07:53:10 -0700

    Add web build

---
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/timsneath/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/timsneath/git/flutter_flip_view/example"
+export "FLUTTER_TARGET=/Users/timsneath/git/flutter_flip_view/example/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/timsneath/flutter/bin/cache/artifacts/engine/ios"
